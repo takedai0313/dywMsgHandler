17ecdb8acaa09be36fbaecbd742eb2d530a1a95b
takedai0313 <takedai@live.jp>
2018-01-01 19:11:53 +0000

    add func that invoke another lambda

--- dywMsgHandler/index.js
+++ dywMsgHandler/index.js
@@ -1,3 +1,13 @@
+'use strict';
+
+const fs = require("fs");
+const invokeLambda = (JSON.parse(fs.readFileSync(process.env.setting, 'utf8'))).invokeLambda;
+const AWS = require("aws-sdk");
+const lambda = new AWS.Lambda({
+    apiVersion: '2015-03-31',
+    region: 'ap-northeast-1'
+});
+
 exports.handler = (event, context, callback) => {
     console.log(event);
     const msgType = event.events[0].message.type;
@@ -6,6 +16,15 @@ exports.handler = (event, context, callback) => {
         console.log("image");
     }else if(msgType === "text"){
         // text系サービスを呼び出す
+        const param = {
+            FunctionName: invokeLambda,
+            InvocationType: "Event",
+            Payload: new Buffer(JSON.stringify(event)).toString()
+        };
+        lambda.invoke(param, (err, data) => {
+          if(err)   console.log(err, err.stack);
+          else      console.log(data);
+        });
         console.log("text");
     }else{
         // なにもしない
